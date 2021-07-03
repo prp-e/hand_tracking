7da36120c6137b5d770b0e4ca24ceadcd9375980
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-03 22:04:41 +0430

    Hands detected perfectly

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ mp_hands = mp.solutions.hands
 
 camera = cv2.VideoCapture(1)
 
-with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands = 1) as hands:
     while camera.isOpened():
         _, image = camera.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False 
+        results = hands.process(image)
 
-        cv2.imshow("Camera No. 1", image)
+        print(results.multi_hand_landmarks)
+
+        image.flags.writeable = True
+        cv2.imshow("Camera No. 1", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
             break 
 
