dfa614ecbdd098ab13f3228b0ea82f1cf0671dff
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-03 22:22:35 +0430

    Tracking hands done

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 camera = cv2.VideoCapture(1)
+print(camera.get(3))
+print(camera.get(4))
 
 with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands = 1) as hands:
     while camera.isOpened():
@@ -18,7 +20,7 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, m
         if results.multi_hand_landmarks:
             for hand_mark in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(image, hand_mark, mp_hands.HAND_CONNECTIONS)
-                print(f'X: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x}\nY: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y}')
+                print(f'X: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 1280}\nY: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 720}')
             
         cv2.imshow("Camera No. 1", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
