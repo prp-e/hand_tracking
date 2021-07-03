2e7af360814843295872fb2f5e37765bb84a0cb5
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-03 22:14:17 +0430

    Got the coordinates of the index finger, perfectly

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, m
         image.flags.writeable = False 
         results = hands.process(image)
 
-        print(results.multi_hand_landmarks)
-
         image.flags.writeable = True
+        #print(results.multi_hand_landmarks)
+        if results.multi_hand_landmarks:
+            for hand_mark in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(image, hand_mark, mp_hands.HAND_CONNECTIONS)
+                print(f'X: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x}\nY: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y}')
+            
         cv2.imshow("Camera No. 1", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
             break 
