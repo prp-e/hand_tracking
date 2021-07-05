48f859ec55e6fc07c6dd279a300038fc245cef3b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-05 13:24:17 +0430

    Let's have some fun

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, m
         image.flags.writeable = True
         #print(results.multi_hand_landmarks)
         if results.multi_hand_landmarks:
-            for hand_mark in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand_mark, mp_hands.HAND_CONNECTIONS)
-                print(f'X: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 1280}\nY: {hand_mark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 720}')
+            for id, landmark in results.multi_hand_landmarks:
+                print(f'{id} : {landmark} ')
             
         cv2.imshow("Camera No. 1", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
