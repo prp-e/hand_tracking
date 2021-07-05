d583e54c1b8ed852111a3e197fe0a99b5b75ae80
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-05 13:30:42 +0430

    Now it seems a little bit more fun

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, m
         image.flags.writeable = True
         #print(results.multi_hand_landmarks)
         if results.multi_hand_landmarks:
-            for id, landmark in results.multi_hand_landmarks:
-                print(f'{id} : {landmark} ')
+            for id, landmark in enumerate(results.multi_hand_landmarks):
+                print(f'{id} : {landmark}\n-----\n')
             
         cv2.imshow("Camera No. 1", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         if cv2.waitKey(1) & 0xff == ord('q'):
